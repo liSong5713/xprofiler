5913c455e412e9291e04c7d27cbe0c9df971f563
rick.li <rick.li@ly.com>
2022-06-09 20:49:53 +0800

    feat: 增加打包后上传到efc功能

--- scripts/upload.js
+++ scripts/upload.js
@@ -1,85 +1,63 @@
 'use strict';
 
 const fs = require('fs');
-const os = require('os');
 const path = require('path');
-const crypto = require('crypto');
-const urllib = require('urllib');
-const formstream = require('formstream');
-const tunnel = require('tunnel-agent');
-const pack = require('../package.json');
-
-const server = process.env.UPLOAD_SERVER;
-const filepath = path.join(__dirname, '../build/Release/xprofiler.node');
-const keyid = process.env.UPLOAD_KEY_ID;
-const keysecret = process.env.UPLOAD_KEY_SECRET;
-
-// check args
-if (!server || !keyid || !keysecret) {
-  console.error('参数错误: server, keyid , keysecret 必填');
-  return;
+const request = require('request');
+const dirname = path.join(__dirname, '../release');
+const EFC_URL = 'http://efs.corp.elong.com/efcapi/upLoad';
+const DOMAIN = '/m.elongstatic.com/xprofiler';
+function base64Encode(file) {
+  return file.toString('base64');
 }
 
-// check filepath
-fs.stat(filepath, function (err, stat) {
-  if (err) {
-    console.error(`文件 ${filepath} 不存在: ${err.message}`);
-    return;
+function checkFile(filepath) {
+  try {
+    const stat = fs.statSync(filepath);
+    if (stat.size <= 0) {
+      console.error(`文件 ${filepath} 为空文件`);
+      return false;
+    }
+    return true;
+  } catch (error) {
+    return false;
   }
+}
 
-  if (stat.size <= 0) {
-    console.error(`文件 ${filepath} 为空文件`);
-    return;
-  }
+function readdir(dirname) {
+  const files = fs.readdirSync(dirname);
+  return files
+    .filter((name) => /\w.tar.gz$/gi.test(name))
+    .filter((filename) => checkFile(path.join(dirname, filename)));
+}
 
-  const form = formstream();
-  form.file('file', filepath, filepath, stat.size);
-
-  const nonce = '' + (1 + parseInt((Math.random() * 100000000000), 10));
-  // get signature
-  const shasum = crypto.createHash('sha1');
-  const timestamp = Date.now();
-  shasum.update([keyid, keysecret, nonce, timestamp].join(''));
-  const sign = shasum.digest('hex');
-
-  const url = 'http://' + server + '/file_upload?keyid=' + keyid +
-    '&nonce=' + nonce + '&sign=' + sign + '&timestamp=' + timestamp +
-    '&version=' + pack.version + '&modules=' + process.versions.modules +
-    '&platform=' + os.platform() + '&arch=' + os.arch() + '&filename=xprofiler.node';
-
-  let agent = false;
-  if (process.env.http_proxy) {
-    const parts = process.env.http_proxy.split(':');
-    agent = tunnel.httpOverHttp({
-      proxy: {
-        host: parts[0],
-        port: parts[1]
-      }
-    });
-  }
-
-  const opts = {
-    dataType: 'json',
-    type: 'POST',
-    timeout: 60000 * 20,
-    headers: form.headers(),
-    stream: form,
-    agent: agent
+function upload(filepath) {
+  const { base: filename } = path.parse(filepath);
+  const [, version] = filename.split(/-/gi);
+  const file = fs.readFileSync(filepath);
+  const options = {
+    url: EFC_URL,
+    method: 'POST',
+    form: {
+      domain: DOMAIN,
+      file: path.join(DOMAIN, version, filename),
+      isunzip: 0,
+      filebuffer: base64Encode(file),
+    },
   };
-
-  urllib.request(url, opts, function (err, data, res) {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    if (res.statusCode !== 200) {
-      console.error({ statusCode: res.statusCode, data: JSON.stringify(data) });
-      return;
-    }
-    if (!data.ok) {
-      console.error(`上传失败: ${JSON.stringify(data)}`);
-      return;
-    }
-    console.log(JSON.stringify(data));
+  return new Promise((resolve, reject) => {
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        console.log(body);
+        return resolve(body);
+      }
+      reject(error);
+      console.log(error);
+    });
   });
-});
+}
+function main(dirname) {
+  const files = readdir(dirname);
+  files.forEach((filename) => upload(path.join(dirname, filename)));
+}
+
+main(dirname);
